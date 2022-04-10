bd5271ab4d1ccf7f2829d7a6d0b7981fd7bee635
Uchenna Amadasun <73265634+uamadasun@users.noreply.github.com>
2022-04-09 22:15:04 -0700

    Add files via upload

---
+++ block.py
@@ -0,0 +1,14 @@
+# Uchenna Amadasun
+# I am a mom of 2
+top = """
+U   U      A
+U   U     A A  
+U   U    A   A
+U   U    AAAAA
+U   U    A   A
+U   U    A   A
+ UUU     A   A
+""" 
+
+
+print (top)
